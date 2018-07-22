2f87787d4ca0f8bd772aae630774c2d04b3bbdca
Denis <ddujota@internationalsafety.com>
2018-07-22 00:11:46 -0400

    scaffold a general design pattern

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(e) {
+  const loadSchedule = () => {
+    // clear the table container
+    // go to db and grab the data,
+    // build the table for the UI
+    // Insert the table into the UI
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // build up the object that will be saved into the DB
+    // save the data on the db
+    // update the view with the new data
+    loadSchedule();
+  };
+});
